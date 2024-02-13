97e6d4f279ae4eb75bbd1bc6a3aef8446894919b
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-02-13 15:57:22 +0800

    defined controller to CREATE a blog

--- src/controllers/blogs.ts
+++ src/controllers/blogs.ts
@@ -7,3 +7,20 @@ const getAllBlogs = async (_req: Request, res: Response, next: NextFunction) => 
     next(error);
   }
 };
+
+// CREATE a blog
+const createBlog = async (req: Request, res: Response, next: NextFunction) => {
+  const validatedBlog = toNewBlog(req.body);
+
+  const newBlog = new BlogModel({
+    ...validatedBlog
+  });
+
+  try {
+    const savedBlog = await newBlog.save();
+    res.json(savedBlog);
+  } catch(error) {
+    next(error);
+  }
+
+};
