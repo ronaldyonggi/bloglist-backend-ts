9877696a28d5c2bbd901f29be90a329ca95182eb
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-02-13 16:25:51 +0800

    4.2 set up mongoDB connection

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+// Initiate MongoDB connection
+mongoose.set('strictQuery', false);
+logger.info('connecting to', config.MONGODB_URI!);
+mongoose.connect(config.MONGODB_URI!)
+  .then(() => logger.info('connected to MongoDB'))
+  .catch((error: unknown) => {
+    if (error instanceof Error) {
+      logger.error('error connecting to MongoDB:', error.message);
+    }
+  });
