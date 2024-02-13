4e1a10379936f26eb998c0f10ec9921a671e00c4
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-02-13 16:08:22 +0800

    changed from using async-await to using Promises

--- src/controllers/blogs.ts
+++ src/controllers/blogs.ts
@@ -3,33 +3,27 @@ import BlogModel from '../models/blog';
 import toNewBlog from "../utils/utils";
 
 // GET all blogs
-const getAllBlogs = async (_req: Request, res: Response, next: NextFunction) => {
-  try {
-    const blogs = await BlogModel.find({});
-    res.json(blogs);
-  } catch(error) {
-    next(error);
-  }
+const getAllBlogs = (_req: Request, res: Response, next: NextFunction) => {
+
+  BlogModel.find({})
+    .then(blogs => res.json(blogs))
+    .catch(error => next(error));
 };
 
 // CREATE a blog
-const createBlog = async (req: Request, res: Response, next: NextFunction) => {
+const createBlog = (req: Request, res: Response, next: NextFunction) => {
   const validatedBlog = toNewBlog(req.body);
 
   const newBlog = new BlogModel({
     ...validatedBlog
   });
 
-  try {
-    const savedBlog = await newBlog.save();
-    res.json(savedBlog);
-  } catch(error) {
-    next(error);
-  }
-
+  newBlog.save()
+    .then(savedBlog => res.json(savedBlog))
+    .catch(error => next(error));
 };
 
-export {
+export default {
   getAllBlogs,
   createBlog
 };
