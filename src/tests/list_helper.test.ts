ff86fd45abff776ea570bcac206d76dd761ab1d7
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-02-13 18:18:28 +0800

    moved initial blog and validation into test block

--- src/tests/list_helper.test.ts
+++ src/tests/list_helper.test.ts
@@ -12,21 +12,21 @@ test('dummy returns one', () => {
 });
 
 describe('total likes', () => {
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-      likes: 5,
-      __v: 0
-    }
-  ];
-
-  // Validate the blog list with typescript first
-  const validatedBlog = listWithOneBlog.map(b => toNewBlog(b));
 
   test('when list has only one blog, equals the likes of that', () => {
+    const listWithOneBlog = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 5,
+        __v: 0
+      }
+    ];
+
+    // Validate the blog list with typescript first
+    const validatedBlog = listWithOneBlog.map(b => toNewBlog(b));
     const result = totalLikes(validatedBlog);
     expect(result).toBe(5);
   });
