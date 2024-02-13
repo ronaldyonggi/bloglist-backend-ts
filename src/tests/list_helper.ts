561ca48b8f5346e8534871714c15ca9a7c98fcf4
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-02-13 18:03:31 +0800

    4.3 created dummy function that returns 1

---
+++ src/tests/list_helper.ts
@@ -0,0 +1,10 @@
+import { Blog } from "../types/blog";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const dummy = (_blogs: Blog[]) => {
+  return 1;
+};
+
+export {
+  dummy
+};
