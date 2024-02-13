570fcd06d8f1995c0771ddb27e5e21bb8d613197
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-02-13 16:31:06 +0800

    4.2 export router

--- src/router/blogsRouter.ts
+++ src/router/blogsRouter.ts
@@ -5,4 +5,6 @@ const router = Router();
 // Base router
 router.route('/')
   .get(blogController.getAllBlogs)
-  .post(blogController.createBlog)
+  .post(blogController.createBlog);
+
+export default router;
