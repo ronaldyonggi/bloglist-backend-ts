31df646c0f933e5914936cc30bb5351256cb03b0
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-02-17 17:04:40 +0800

    prettier formatting

--- src/tests/blog_api.tests.ts
+++ src/tests/blog_api.tests.ts
@@ -1,15 +1,15 @@
 import { initialBlogs } from './initialBlogs';
 import BlogModel from '../models/blog';
-import supertest from "supertest";
-import app from "../app";
-import mongoose from "mongoose";
+import supertest from 'supertest';
+import app from '../app';
+import mongoose from 'mongoose';
 
 const api = supertest(app);
 
 beforeEach(async () => {
   await BlogModel.deleteMany({});
   const blogObjects = initialBlogs.map((b) => new BlogModel(b));
-  const promiseArray = blogObjects.map(b => b.save());
+  const promiseArray = blogObjects.map((b) => b.save());
   await Promise.all(promiseArray);
 });
 
