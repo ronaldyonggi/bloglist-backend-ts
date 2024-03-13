774f685f4d674eca899e7eb31ac7b27623926665
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-03-13 15:03:29 +0800

    defined validateNewUser type narrowing function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,5 @@
 import { NewBlog } from "../types/blog";
+import { NewUser } from "../types/user";
 
 // String type guard
 const isString = (text: unknown): text is string => {
@@ -52,8 +53,27 @@ const validateNewBlog = (object: unknown): NewBlog => {
 
   throw new Error('TypeScript validateNewBlog error: invalid input data!');
 };
+
+// Validate request for creating a new user
+const validateNewUser = (object: unknown): NewUser => {
+  if (!object || typeof object !== 'object') {
+    throw new Error('TypeScript validateNewUser error: incorrect or missing data');
+  }
+
+  if ('username' in object && 'name' in object && 'password' in object) {
+    const newUser = {
+      username: parseString(object.username),
+      name: parseString(object.name),
+      password: parseString(object.password)
+    };
+
+    return newUser;
+  }
+
+  throw new Error('TypeScript validateNewUser error: invalid input data!');
 };
 
 export default {
   validateNewBlog,
+  validateNewUser
 };
