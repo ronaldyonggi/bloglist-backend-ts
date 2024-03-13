4cb75629d3ddb114caf3b29f33ba743f05bc482d
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-03-13 12:50:35 +0800

    controllers now use async/await

--- src/controllers/blogs.ts
+++ src/controllers/blogs.ts
@@ -5,31 +5,28 @@ import toNewBlog from "../utils/utils";
 const blogsRouter = Router();
 
 // GET all blogs
-blogsRouter.get('/', (_req: Request, res: Response, next: NextFunction) => {
-
-  BlogModel.find({})
-    .then(blogs => res.json(blogs))
-    .catch(error => next(error));
+blogsRouter.get('/', async (_req: Request, res: Response) => {
+  const blogs = await BlogModel.find({});
+  res.json(blogs);
 });
 
 // CREATE a blog
-blogsRouter.post('/', (req: Request, res: Response, next: NextFunction) => {
+blogsRouter.post('/', async (req: Request, res: Response) => {
   const validatedBlog = toNewBlog(req.body);
 
   const newBlog = new BlogModel({
     ...validatedBlog
   });
 
-  newBlog.save()
-    .then(savedBlog => res.json(savedBlog))
-    .catch(error => next(error));
+  const savedBlog = await newBlog.save();
+  res.json(savedBlog);
 });
 
 // DELETE a blog
-blogsRouter.delete('/:id',  (req: Request, res: Response, next: NextFunction) => {
-  BlogModel.findByIdAndDelete(req.params.id)
-    .then(() => res.status(204).end())
-    .catch(error => next(error));
+blogsRouter.delete('/:id', async (req: Request, res: Response) => {
+  await BlogModel.findByIdAndDelete(req.params.id);
+  res.status(204).end();
+});
 });
 
 export default blogsRouter;
